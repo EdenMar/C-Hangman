38a362cbef10d99d2ffcb2f86a7101d3094e5c2e
Eden Mar <emar@ualberta.ca>
2017-09-08 21:48:14 -0600

    Initial commit

---
+++ Hangman/Hangman.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eden
+ * Date: 2017-09-06
+ * Time: 6:17 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman
+{
+	
+	public class Gameboard{
+		private int limit = 7;
+		private int guesses;
+		private String secretWord;
+		private StringBuilder currentWord = new StringBuilder();
+			
+		public Gameboard(String word){
+			guesses = 0;
+			this.secretWord = word;
+			currentWord.Append(Convert.ToChar("_"), secretWord.Length);
+			
+		}
+		
+		public void printGameboard(){
+			Console.Write("The word length is ");
+			Console.Write(this.getSecretWordLength());
+			Console.WriteLine(" letters long.");
+			Console.WriteLine(currentWord.ToString());
+		}
+		
+		public bool hasGuessesLeft(){
+			return guesses < limit;
+		}
+		
+		public void tryLetter(char c){
+			
+		}
+		
+		public int getSecretWordLength(){
+			return secretWord.Length;
+		}
+
+	}
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var wordList = new Dictionary<int, String>{
+				{0, "apple"},
+				{1, "banana"},
+				{2, "cantelope"},
+				{3, "durian"},
+				{4, "grapefruit"},
+				{5, "mango"}
+			};
+			Console.WriteLine("Welcome to Hangman.");
+			
+			Random r = new Random();
+			int val = r.Next(6);
+			String secret = wordList[val];
+			Gameboard game = new Gameboard(secret);
+			Console.WriteLine("You have 7 guesses left.");
+			game.printGameboard();
+			while (game.hasGuessesLeft()){
+				String s = Console.ReadLine();
+				char c = s[0];
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Hangman/HangmanTest.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eden
+ * Date: 2017-09-06
+ * Time: 6:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using NUnit.Framework;
+using Hangman;
+
+namespace HangmanTest
+{
+    [TestFixture]
+    public class HangmanTest
+    {
+        [Test]
+        public void Gameboard_TestSetup()
+        {
+        	Gameboard g = new Gameboard("banana");
+        	g.printGameboard();
+        	Assert.AreEqual(g.getSecretWordLength(), 6);
+        }
+        
+        [Test]
+        public void Gameboard_TestTryLetter(){
+        	Gameboard g = new Gameboard("banana");
+        	Assert.Fail();
+        }
+    }
+}
